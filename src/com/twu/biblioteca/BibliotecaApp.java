6ecc5448f61a054c70a6fba9ff8287eb3e5d2e9d
廖雪青 <liaoxueqing@liaoxueqingdeMac-mini.local>
2019-04-12 11:12:54 +0800

    Show books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,10 +1,36 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
+    private List<Book> books;
 
     public static void main(String[] args) {
         welcome();
+        BibliotecaApp app = new BibliotecaApp();
+        List<Book> books = new ArrayList<Book>(){
+            {
+                add(new Book("book1"));
+                add(new Book("book2"));
+                add(new Book("book3"));
+                add(new Book("book4"));
+            }
+        };
+        app.showBooks(books);
+
     }
+
+    public void showBooks(List<Book> books) {
+        for (Book book : books) {
+            printBook(book);
+        }
+    }
+
+    private void printBook(Book book) {
+        System.out.println(book.getName());
+    }
+
     public static void welcome() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,15 +1,22 @@
 package com.twu.biblioteca;
 
-import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class BibliotecaAppTest {
+    BibliotecaApp app;
+    @Before
+    public void setUp() throws Exception {
+        app =  new BibliotecaApp();
+    }
 
     @Test
     public void welcome() {
@@ -20,6 +27,29 @@ public class BibliotecaAppTest {
 
         BibliotecaApp.welcome();
         assertThat(outContent.toString(), is("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n"));
+
+        System.setOut(old);
+    }
+
+    @Test
+    public void showBooks() {
+        List<Book> books = new ArrayList<Book>(){
+            {
+                add(new Book("book1"));
+                add(new Book("book2"));
+                add(new Book("book3"));
+                add(new Book("book4"));
+            }
+        };
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+        PrintStream old = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        app.showBooks(books);
+
+        assertThat(outContent.toString(), is("book1\nbook2\nbook3\nbook4\n"));
 
         System.setOut(old);
     }
